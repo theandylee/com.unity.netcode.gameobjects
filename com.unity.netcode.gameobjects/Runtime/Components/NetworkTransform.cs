7e9f0fc064c0ddf7dc0083c7af807a70d73dbb4c
Andy Lee <xatariboyx@gmail.com>
2025-11-03 18:23:16 +0100

    NetworkTransform: update internal position

--- com.unity.netcode.gameobjects/Runtime/Components/NetworkTransform.cs
+++ com.unity.netcode.gameobjects/Runtime/Components/NetworkTransform.cs
@@ -1002,8 +1002,6 @@ namespace Unity.Netcode.Components
 
         #region PROPERTIES AND GENERAL METHODS
 
-        public bool SkipNextStateUpdate { get; set; }
-
         /// <summary>
         /// Used on the authority side only.
         /// This is the current network tick and is set within <see cref="NetworkManager.NetworkUpdate(NetworkUpdateStage)"/>.
@@ -2692,7 +2690,10 @@ namespace Unity.Netcode.Components
                 }
                 else
                 {
+                    m_PositionInterpolator.InterpolateState.Reset(transform.position);
+                    m_RotationInterpolator.InterpolateState.Reset(transform.rotation);
                     m_InternalCurrentPosition = transform.position;
+                    m_InternalCurrentRotation = transform.rotation;
                 }
 
                 if (SynchronizeScale)
@@ -2807,16 +2808,13 @@ namespace Unity.Netcode.Components
                 else
 #endif
                 {
-                    if (!SkipNextStateUpdate)
+                    if (PositionInLocalSpace)
                     {
-                        if (PositionInLocalSpace)
-                        {
-                            transform.localPosition = m_InternalCurrentPosition;
-                        }
-                        else
-                        {
-                            transform.position = m_InternalCurrentPosition;
-                        }
+                        transform.localPosition = m_InternalCurrentPosition;
+                    }
+                    else
+                    {
+                        transform.position = m_InternalCurrentPosition;
                     }
                 }
             }
@@ -2852,21 +2850,16 @@ namespace Unity.Netcode.Components
                 else
 #endif
                 {
-                    if (!SkipNextStateUpdate)
+                    if (RotationInLocalSpace)
                     {
-                        if (RotationInLocalSpace)
-                        {
-                            transform.localRotation = m_InternalCurrentRotation;
-                        }
-                        else
-                        {
-                            transform.rotation = m_InternalCurrentRotation;
-                        }
+                        transform.localRotation = m_InternalCurrentRotation;
+                    }
+                    else
+                    {
+                        transform.rotation = m_InternalCurrentRotation;
                     }
                 }
             }
-
-            SkipNextStateUpdate = false;
 
             // Apply the scale if we are synchronizing scale
             if (SynchronizeScale)
